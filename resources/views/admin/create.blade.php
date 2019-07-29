fd2bda3e35a86f8a8703a272f2436310a439067e
John Biddulph <john.mbiddulph@gmail.com>
2019-07-29 23:31:42 +0100

    blog system part3 144

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            @include('admin.left-menu')
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Menu</div>
+                <div class="card-body">
+                    <form action="{{route('post.store')}}" method="POST" enctype="multipart/form-data">@csrf
+                        <div class="form-group">
+                            <label for="Title">Title</label>
+                            <input type="text" name="title" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="Description">Description</label>
+                            <textarea name="content" id="" cols="30" rows="6" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="Image">Image</label>
+                            <input type="file" name="image" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="Status">Status</label>
+                            <select name="status" id="" class="form-control">
+                                <option value="0">Draft</option>
+                                <option value="1">Published</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,51 +1,46 @@
 @extends('layouts.app')
-
 @section('content')
-
 <div class="container">
+    @if(Session::has('message'))
+        <div class="alert alert-success">
+            {{Session::get('message')}}
+        </div>
+    @endif
     <div class="row">
         <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">Menu</div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item"><a href="home">Home</a></li>
-                        <li class="list-group-item"><a href="home">Home</a></li>
-                        <li class="list-group-item"><a href="home">Home</a></li>
-                    </ul>
-                </div>
-            </div>
+            @include('admin.left-menu')
         </div>
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Menu</div>
-                <div class="card-body">
-                    <form action="">
-                        <div class="form-group">
-                            <label for="Title">Title</label>
-                            <input type="text" name="title" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="Description">Description</label>
-                            <textarea name="description" id="" cols="30" rows="6" class="form-control"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="Image">Image</label>
-                            <input type="file" name="image" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="Status">Status</label>
-                            <select name="status" id="" class="form-control">
-                                <option value="0">Draft</option>
-                                <option value="1">Published</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-success">Submit</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
